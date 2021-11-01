bff9bf2c1b0ace8b54f78363f02522b7b540e3ef
atharv123-wq <atharvamaid@gmail.com>
2021-11-01 11:05:34 +0530

    retrieve data from firebase

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Main from "../audio/Main";
-import { db } from "../../firebase/config";
+import { firestore } from "../../firebase/config";
 
 
 
 function DashBoard() {
 
 
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(async()=>{
+            await firestore.collection("sections").doc("Technical").collection("questions").get().then((snapShot)=>{
+                var data = [];
+                    snapShot.forEach((doc)=>{
+                        data.push(doc.data()['text']);
+                    });
+                    setQuestions(data);
+            }).catch((error)=>{
+                console.log(error);
+            })
+    },[])
 
     return (
         <div className="container">
             <Main/>
+            
         </div>
     );
 }
