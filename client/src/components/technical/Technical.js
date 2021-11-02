f48d58744b868c9522af0e89ebb0d5f8476ea522
atharv123-wq <atharvamaid@gmail.com>
2021-11-02 18:58:01 +0530

    added report generate modal

--- client/src/components/technical/Technical.js
+++ client/src/components/technical/Technical.js
@@ -3,6 +3,22 @@ import React, {useEffect, useRef, useState} from 'react';
 import {firestore} from '../../firebase/config';
 import Sound from "react-sound";
 import useInterval from ".././audio/useInterval";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 function Technical() {
@@ -10,6 +26,11 @@ function Technical() {
 	const [questions, setQuestions] = useState([]);	
 	const [mediaRecorder, setMediaRecorder] = useState(null);
 	const [ind, setInd] = useState(0)
+	const [open , setOpen] = useState(false);
+
+	const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
 	let startTime = 0
 	let endTime = 0
 
@@ -133,6 +154,7 @@ const calculatePercentage = ()=>{
 				}
 			}
 			console.log(count)
+			handleOpen();
 		}else{
 			console.log("")
 		}
@@ -247,28 +269,23 @@ const playStop = (index) => {
 			);
 		})}
 		</div>
-		<button type="button"  className="btn btn-primary report-button" data-toggle="modal" data-target="#exampleModal" onClick={()=>calculatePercentage()}>Generate Report</button>
+		<button type="button"  className="btn btn-primary report-button" data-toggle="modal" data-target="#myModal" onClick={()=>calculatePercentage()}>Generate Report</button>
 		</section>
-
-		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-					...
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
-				</div>
-				</div>
-			</div>
-			</div>
+<Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography style={{color:"black"}} id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography style={{color:"black"}} id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </div>
     )
 }
